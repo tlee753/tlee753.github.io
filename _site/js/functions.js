5fde20dfd854bee230ca0fcb228e4a155eefa201
Tyler Lee <tlee753@gmail.com>
2016-06-04 22:58:31 -0500

    links
    Former-commit-id: 1ae1245556d13cd1671d408c77bdcf179db5e42e

--- _site/js/functions.js
+++ _site/js/functions.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-   // code here 
+   smoothScroll(1000);
 });
+
+function smoothScroll (duration) {
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, duration);
+        }
+    });
+}
