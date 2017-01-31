33670b734439d80a88778b4a6f35cf295c315134
Tyler Lee <tlee753@gmail.com>
2017-01-31 16:13:48 +0100

    update website
    Former-commit-id: 35dfb0a0e6c04e2c4fd47dc0cb4a20e0469e1055
    Former-commit-id: 2ef309e38cfa04b361ce2df50524a40d0149a3dd

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,8 +8,12 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("slider-image");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
