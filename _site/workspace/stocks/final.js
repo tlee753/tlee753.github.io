73a9b17719919799d9b32797a1229a76b4675ac0
Tyler Lee <tlee753@gmail.com>
2017-03-13 11:52:33 +0100

    update website
    Former-commit-id: 6496fea22b69309ff9189b5d7b8c6ac480573e73
    Former-commit-id: d78f5cc0b728d0b14d9b5607c34b41dac7111d85

---
+++ _site/workspace/stocks/final.js
@@ -0,0 +1,14 @@
+function getData(ticker) {
+    var url = "http://query.yahooapis.com/v1/public/yql";
+    //var symbol = $(ticker).val();
+    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + ticker + "')");
+
+    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+        .done(function (data) {
+            $("#result").text(ticker.toUpperCase() + ": $" + data.query.results.quote.LastTradePriceOnly);
+        })
+        .fail(function (jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            $("#result").text('Request failed: ' + err);
+        });
+}
